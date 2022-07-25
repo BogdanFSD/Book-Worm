48d3340d1b7a1e5af9cb02b4a2769e0c19dd9d3a
Bogdandev <jobsnospam007@gmail.com>
2022-07-25 01:26:45 +0000

    correct and wrong assignor created

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,5 +33,19 @@ function resetState(){
 }
 
 function showQuestion(){
-    
+
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+      element.classList.add('correct');
+    } else {
+      element.classList.add('wrong');
+    }
+  }
+
+  function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+  }
