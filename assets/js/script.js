fd4d212abcfb986e0134efac21011200ab424473
Bogdandev <jobsnospam007@gmail.com>
2022-07-25 01:19:33 +0000

    function that take wuestions to the front

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 /* jshint esversion: 8 */
-
+/**initialize const */
 const startBtn = document.getElementById('start-btn');
+const questionContainer = document.querySelector('.question-container');
+const mainContainer = document.querySelector('.container');
+const nextBtn = document.getElementById('next-btn');
 
 
 startBtn.addEventListener('click', startGame);
+
+
+  
+
+function startGame() {
+    mainContainer.classList.add('hide');
+    startBtn.classList.add('hide');
+    nextBtn.classList.remove('hide');
+    questionContainer.classList.remove('hide');
+    mixQuestion = questions.sort(() => Math.random() - .5);
+    QuestionIndex = 0;
+    setNextQuestion();
+}
