152cb9b9fa7ed8b4c7e35ac9068bcd8924423dc0
Bogdandev <jobsnospam007@gmail.com>
2022-07-25 01:27:26 +0000

    Questions created for tutorial

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,42 @@ function setStatusClass(element, correct) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
   }
+
+  const questions = [
+    {
+      question: 'Who is the evil in Harry Potter?',
+      answers: [
+        { text: 'Voldemort', correct: true },
+        { text: 'Severus Snape', correct: false },
+        { text: 'Dobby', correct: false },
+        { text: 'Gandalf', correct: false }
+      ]
+    },
+    {
+      question: 'What happened with Don Quixote?',
+      answers: [
+        { text: 'Miguel de Cervantes', correct: true },
+        { text: 'Mark Twain ', correct: false },
+        { text: 'Cillian Murphy', correct: false },
+        { text: 'Sancho Panza', correct: false }
+      ]
+    },
+    {
+      question: 'What character is Queequeg ?',
+      answers: [
+        { text: 'Captain', correct: false },
+        { text: 'Cannibal ', correct: true },
+        { text: 'Moby Dick', correct: false },
+        { text: 'Pirat', correct: false }
+      ]
+    },
+    {
+      question: 'Because of what Hamlet died?',
+      answers: [
+        { text: 'Car accident', correct: false },
+        { text: 'Poison', correct: false },
+        { text: 'Rapier', correct: false },
+        { text: 'Poisoned rapier', correct: true }
+      ]
+    },
+  ];
