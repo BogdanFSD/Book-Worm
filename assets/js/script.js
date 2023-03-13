9797fc7e5f415cbe536db70c1b6e7fadcbd66db0
Bogdandev <luitko007@gmail.com>
2023-03-13 18:44:58 +0000

    added timer, more questions, changed logic code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,77 @@
-/* jshint esversion: 8 */
-const endBtn =document.getElementById('end-btn');
-const startBtn = document.getElementById('start-btn');
-const nextBtn = document.getElementById('next-btn');
-const questionContainer = document.querySelector('.question-container');
-const questionEl = document.getElementById('question');
-const answerBtnEl = document.getElementById('answer-buttons');
-const mainContainer = document.querySelector('.container');
-const endContainer = document.querySelector('.endContainer');
-const newgame = document.getElementById('newgame-btn');
-const Gamerules =document.getElementById('Gamerules');
-const playBtn = document.getElementById('btn-play');
 
-let mixQuestion, QuestionIndex;
-endBtn.addEventListener('click',lastbtn);
-playBtn.addEventListener('click',startGame);
-newgame.addEventListener('click',gameRules);
-startBtn.addEventListener('click', gameRules);
-nextBtn.addEventListener('click', () => {
-  QuestionIndex++;
+// start button
+const mainContainer = document.querySelector('.container');
+const start = document.querySelector(".start-btn");
+const container_styling = document.getElementById("container_styling")
+
+// game rules section
+const gamerules = document.getElementById('Gamerules')
+const play = document.querySelector(".btn-play");
+
+// question section
+const timer = document.querySelector(".timer-sec");
+const questionContainer = document.querySelector('.question-container');
+const questionA = document.getElementById('question');
+const answerBtn = document.getElementById('answer-buttons');
+const next = document.querySelector(".next-btn");
+const end = document.querySelector(".end-btn");
+
+// end section
+const endContainer = document.querySelector('.endContainer');
+const newgame = document.querySelector(".newgame-btn");
+
+let quizStartTime = 0;
+let time = 20;
+let countdown;
+let mixedQuestion;
+let questionIndex = 0;
+let score = 0;
+
+start.addEventListener('click', gameRules);
+
+play.addEventListener('click',() => {
+ startTimer();
+ startGame();
+});
+
+next.addEventListener('click', () => {
+  questionIndex++;
   setNextQuestion();
 });
 
-function startGame() {
-  startBtn.classList.add('hide');
-  mainContainer.classList.add('hide');
-  newgame.classList.add('hide');
+end.addEventListener('click', endQuiz);
+
+newgame.addEventListener('click', () => {
   endContainer.classList.add('hide');
-  Gamerules.classList.add('hide');
-  nextBtn.classList.remove('hide');
-  mixQuestion = questions.sort(() => Math.random() - 0.5);
-  QuestionIndex = 0;
-  questionContainer.classList.remove('hide');
-  setNextQuestion();
-}
+  startTimer();
+  startGame();
+});
 
 function gameRules (){
-  Gamerules.classList.remove('hide');
-  mainContainer.classList.add('hide');
-  endContainer.classList.add('hide');
-  endBtn.classList.add('hide'); 
+  gamerules.classList.remove('hide');
+  container_styling.classList.add('hide');
+
+};
+
+function startGame (){
+  quizStartTime = new Date();
+  time = 20;
+  timer.innerHTML = time;
+  end.classList.add('hide')
+  questionContainer.classList.remove('hide');
+  gamerules.classList.add('hide');
+  
+  mixedQuestion = questions.sort(() => Math.random() - 0.5).slice(0, 5);
+  questionIndex = 0;
+  score = 0;
+  showQuestion();
 }
 
-function setNextQuestion() {
-  resetState();
-  showQuestion(mixQuestion[QuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionEl.innerText = question.question;
-  question.answers.forEach(answer => {
+function showQuestion() {
+  const currentQuestion = mixedQuestion[questionIndex];
+  questionA.innerText = currentQuestion.question;
+  answerBtn.innerHTML = '';
+  currentQuestion.answers.forEach(answer => {
     const button = document.createElement('button');
     button.innerText = answer.text;
     button.classList.add('btn');
@@ -56,40 +79,52 @@ function showQuestion(question) {
       button.dataset.correct = answer.correct;
     }
     button.addEventListener('click', selectAnswer);
-    answerBtnEl.appendChild(button);
+    answerBtn.appendChild(button);
   });
+  time = 20;
+  timer.innerHTML = time;
+  startTimer();
+}
+
+function setNextQuestion() {
+  resetState();
+  if (questionIndex < mixedQuestion.length) {
+    
+    showQuestion();
+  } else {
+    endQuiz();
+  }
 }
 
 function resetState() {
   clearStatusClass(document.body);
-  nextBtn.classList.add('hide');
-  while (answerBtnEl.firstChild) {
-    answerBtnEl.removeChild(answerBtnEl.firstChild);
+  next.classList.add('hide');
+  while (answerBtn.firstChild) {
+    answerBtn.removeChild(answerBtn.firstChild);
   }
 }
 
-function selectAnswer(e) {
-  const selBtn = e.target;
-  const correct = selBtn.dataset.correct;
-  setStatusClass(document.body, correct);
-  Array.from(answerBtnEl.children).forEach(button => {
+function selectAnswer(event) {
+  const selectedBtn = event.target;
+  const isCorrect = selectedBtn.dataset.correct;
+  setStatusClass(document.body, isCorrect);
+  Array.from(answerBtn.children).forEach(button => {
     setStatusClass(button, button.dataset.correct);
   });
-  if (mixQuestion.length > QuestionIndex + 2) {
-    nextBtn.classList.remove('hide');
+  if (isCorrect) {
+    score++;
+  }
+  if (questionIndex < mixedQuestion.length - 1) {
+    next.classList.remove('hide');
   } else {
-    endBtn.classList.remove('hide');
-  } 
-}
-function lastbtn (){
-    endContainer.classList.remove('hide'); 
-    newgame.classList.remove('hide');
-    questionContainer.classList.add('hide');
+    end.innerText = `Show my result`
+    end.classList.remove('hide');
+  }
 }
 
-function setStatusClass(element, correct) {
+function setStatusClass(element, isCorrect) {
   clearStatusClass(element);
-  if (correct) {
+  if (isCorrect) {
     element.classList.add('correct');
   } else {
     element.classList.add('wrong');
@@ -100,6 +135,31 @@ function clearStatusClass(element) {
   element.classList.remove('correct');
   element.classList.remove('wrong');
 }
+
+function endQuiz() {
+  clearInterval(countdown); // stop the timer
+  const quizEndTime = new Date();
+  const elapsedTime = (quizEndTime - quizStartTime) / 1000 - 2;
+  
+  questionContainer.classList.add('hide');
+  document.getElementById("presult").innerHTML = `Your Score: ${score} / ${mixedQuestion.length}<br>You spent: ${elapsedTime.toFixed(1)} seconds to solve this quiz.<br> Can you do better?`;
+  endContainer.classList.remove('hide');
+};
+
+function startTimer() {
+  clearInterval(countdown);
+  countdown = setInterval(function() {
+    if (time > 0) {
+      time--;
+      timer.innerHTML = time;
+    }  else  {
+      clearInterval(countdown);
+      questionIndex++;
+      setNextQuestion();
+    }
+  }, 1000);
+}
+
 
 const questions = [
   {
@@ -138,4 +198,157 @@ const questions = [
       { text: 'Poisoned rapier', correct: true }
     ]
   },
+  {
+      question: 'What is the second Harry Potter book called?',
+      answers: [
+        { text: 'Jane Eyre', correct: false },
+        { text: 'Oliver Twist', correct: false },
+        { text: 'Dragon', correct: false },
+        { text: 'Harry Potter & The Chamber of Secrets', correct: true }
+      ]
+    },
+    {
+      question: 'Who wrote Frankenstein?',
+      answers: [
+        { text: 'Mary Shelley', correct: true },
+        { text: 'Danielle Steel', correct: false },
+        { text: 'J. K. Rowling', correct: false },
+        { text: 'Osamu Tezuka', correct: false }
+      ]
+    },
+    {
+      question: 'What wrote Homage to Catalonia?',
+      answers: [
+        { text: 'George Orwell', correct: true },
+        { text: 'Irving Wallace ', correct: false },
+        { text: 'Cillian Murphy', correct: false },
+        { text: 'Sancho Panza', correct: false }
+      ]
+    },
+    {
+      question: 'Who took pen name as Eric Arthur Blair?',
+      answers: [
+        { text: 'Mark Twain', correct: false },
+        { text: 'George Orwell ', correct: true },
+        { text: 'Stan Lee', correct: false },
+        { text: 'Dr. Seuss', correct: false }
+      ]
+    },
+    {
+      question: 'Name the first book in the Noughts and Crosses series?',
+      answers: [
+        { text: 'Noughts & Crosses', correct: false },
+        { text: 'Knife Edge', correct: false },
+        { text: 'Double Cross', correct: false },
+        { text: 'Black & White', correct: true }
+      ]
+    },
+    {
+        question: 'What was the name of Charles Dickens last book which was left unfinished?',
+        answers: [
+          { text: 'The Old Man of Lochnagar', correct: false },
+          { text: 'Oliver Twist', correct: false },
+          { text: 'Charlotte, Emily and Anne Bronte', correct: false },
+          { text: 'The Mystery Of Edwin Drood', correct: true }
+        ]
+    },
+    {
+      question: 'Which famous writer said, "A woman is only a woman, but a good cigar is a smoke"?',
+      answers: [
+        { text: 'Agatha Christie', correct: false },
+        { text: 'Charles Dickens', correct: false },
+        { text: 'William Shakespeare', correct: false },
+        { text: 'Rudyard Kipling', correct: true }
+      ]
+    },
+    {
+      question: 'Joanne Harris wrote which book, now made into a hit film starring Johnny Depp and Juliette Binoche?',
+      answers: [
+        { text: 'Blackberry Wine ', correct: false },
+        { text: 'Chocolat', correct: true },
+        { text: 'Gentlemen & Players ', correct: false },
+        { text: 'The Lollipop Shoes', correct: false }
+      ]
+    },
+    {
+      question: 'Which travel writer comes from De Moines, Iwoa?',
+      answers: [
+        { text: 'Robert Ludlum ', correct: false },
+        { text: 'Bill Bryson', correct: true },
+        { text: ' Enid Blyton ', correct: false },
+        { text: 'Alistair Maclean', correct: false }
+      ]
+    },
+    {
+      question: 'Who created Miss Marple?',
+      answers: [
+        { text: 'Rosemary Conley ', correct: false },
+        { text: 'Bill Bryson', correct: false },
+        { text: ' Douglas Adams ', correct: false },
+        { text: 'Agatha Christie', correct: true }
+      ]
+    },
+    {
+      question: 'What was the first book in English to be printed in England?',
+      answers: [
+        { text: 'Crime and Punishement ', correct: false },
+        { text: 'The Canterbury tales', correct: true },
+        { text: ' Harry Potter ', correct: false },
+        { text: 'Mary Poppins', correct: false }
+      ]
+    },
+    {
+      question: 'Who was responsible for the Complete Hip and Thigh Diet?',
+      answers: [
+        { text: 'The Scarlet Pimpernel ', correct: false },
+        { text: 'Macbeth', correct: false },
+        { text: ' Rosemary Conley ', correct: true },
+        { text: ' John Bunyan', correct: false }
+      ]
+    },
+    {
+      question: 'What type of book is the OED?',
+      answers: [
+        { text: 'Dictionary ', correct: true },
+        { text: 'List', correct: false },
+        { text: ' Kindle ', correct: false },
+        { text: 'Tuple', correct: false }
+      ]
+    },
+    {
+      question: 'According to Sue Townsend how old was Adrian Mole when he first started writing his diary?',
+      answers: [
+        { text: '15', correct: false },
+        { text: '13', correct: true },
+        { text: '43', correct: false },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Crockfords is a reference book relating to which group of people?',
+      answers: [
+        { text: 'Punks ', correct: false },
+        { text: 'Clergy of the church of England', correct: true },
+        { text: 'Football funs', correct: false },
+        { text: 'Prisoners', correct: false }
+      ]
+    },
+    {
+      question: 'If you were reading a book published by Fodor, what would the subject be?',
+      answers: [
+        { text: 'Travel ', correct: true },
+        { text: 'Painting', correct: false },
+        { text: 'War', correct: false },
+        { text: 'Friendship', correct: false }
+      ]
+    }
 ];
+
+
+
+
+
+
+
+
+
